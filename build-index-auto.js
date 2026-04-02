d3ff7a325c37d8faa399a0679a375408ce83ac30
CK-henry <henrydiaosi@gmail.com>
2026-04-03 02:29:19 +0800

    补充

--- build-index-auto.js
+++ build-index-auto.js
@@ -1,3 +1,16 @@
 #!/usr/bin/env node
 
-require('./src/tools/build-index.js');
+const fs = require('fs');
+const path = require('path');
+
+const rootDir = __dirname;
+const cjsEntry = path.join(rootDir, 'build-index-auto.cjs');
+const distEntry = path.join(rootDir, 'dist', 'tools', 'build-index.js');
+
+if (fs.existsSync(cjsEntry)) {
+  require(cjsEntry);
+} else if (fs.existsSync(distEntry)) {
+  require(distEntry);
+} else {
+  throw new Error('Cannot find build index entry. Expected build-index-auto.cjs or dist/tools/build-index.js');
+}
